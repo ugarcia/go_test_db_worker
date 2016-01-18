a7ea2f6f4bb6c05c5d420f73e5fc27f0637bc9a9
unai <unai.gg77@gmail.com>
2016-01-18 19:53:51 +0100

    improved inheritance from golang directly

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+IMAGE_NAME=go_test_db_worker
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 HOME_DIR="${SCRIPT_DIR}/.."
-docker build "$@" -t go_test_db_worker $HOME_DIR
+cp ~/.ssh/id_rsa ${HOME_DIR}/id_rsa
+docker build "$@" -t $IMAGE_NAME $HOME_DIR
+rm -rf ${HOME_DIR}/id_rsa
